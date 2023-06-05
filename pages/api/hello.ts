80b9779154863bdfa81aba066f8779830c0acbf0
suzze87 <suzzenelson@gmail.com>
2023-06-04 21:02:47 -0300

    first commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createRouter, expressWrapper } from "next-connect";
+import cors from "cors";
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+
+router.use(async (req, res, next) => {
+    const start = Date.now();
+    await next(); // call next in chain
+    const end = Date.now();
+    console.log(`Request took ${end - start}ms`);
+})
+    .get((req, res) => {
+        res.json({ message: "GET ROUTE" });
+    })
+    .put((req, res) => {
+
+        res.json({ message: "PUT ROUTE" });
+    });
+
+ 
+
+export default router.handler({
+    onError: (err, req, res) => {
+        console.error((err as any).stack);
+        res.status((err as any).statusCode || 500).end((err as any).message);
+    },
+});
